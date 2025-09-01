51dc45757681e46508506066f5e6a683461591d8
Apsara123127 <apsara.aitrich@gmail.com>
2025-09-01 10:55:55 +0530

    added

---
+++ Dot net Core/Chapter 2/Workshop/Workshop 1/Admin_Job/Admin_Job/Interface/IJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin_Job.Manager
+{
+    public interface IJob
+    {
+        public void addjob();
+        public void ListJob();
+    }
+}
