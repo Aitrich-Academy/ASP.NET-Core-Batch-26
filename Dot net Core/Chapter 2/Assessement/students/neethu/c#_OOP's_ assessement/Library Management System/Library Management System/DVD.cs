1a690b1d945fa4a770b7a7131e2e458083215f18
Neethu <neethukottoor@gmail.com>
2025-09-11 10:27:06 +0400

    added

---
+++ Dot net Core/Chapter 2/Assessement/students/neethu/c#_OOP's_ assessement/Library Management System/Library Management System/DVD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    public class DVD:LibraryItem
+    {
+        public int Duration { get; set; }
+
+        private string Type = "DVD";
+        
+        public DVD(string itemid,string title,bool isavailable,int duration):base(itemid,title,isavailable)
+        {
+            Duration = duration;
+        }
+        public override string GetItemType(LibraryItem item)
+        {
+            return Type;
+        }
+
+    }
+}
---
+++ Dot net Core/Chapter 2/Assessement/students/neethu/c#_OOP's_ assessement/Library Management System/Library Management System/Program.cs
@@ -0,0 +1,30 @@
+﻿using Library_Management_System;
+
+internal class Program
+{
+     static void Main(string[] args)
+    {
+        LibraryItem[] libraryItems = new LibraryItem[6];
+        libraryItems[0] = new Book("001", "Book1", true, "Author1");
+        libraryItems[1] = new Book("002", "Book2", true, "Author2");
+        libraryItems[2] = new Book("003", "Book3", true, "Author3");
+        libraryItems[3] = new DVD("004", "DVD1", true, 2);
+        libraryItems[4] = new DVD("005", "DVD2", true, 3);
+        libraryItems[5] = new DVD("006", "DVD3", true, 2);
+
+        libraryItems[0].BorrowItem();
+        libraryItems[2].BorrowItem();
+
+        libraryItems[3].BorrowItem();
+        libraryItems[4].BorrowItem();
+
+        libraryItems[2].ReturnItem();
+        libraryItems[4].ReturnItem();
+
+        Console.WriteLine("List of Item's available in the Library");
+        foreach (LibraryItem item in libraryItems)
+        {
+            Console.WriteLine($"Item ID: {item.ItemId} Type: {item.GetItemType(item)} Availability: {item.IsAvailable}");
+        }
+    }
+}
