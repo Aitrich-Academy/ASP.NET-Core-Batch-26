efbd96d046fec184a1b2ad03aec568020f7c20f7
hannethch <hannethasad@gmail.com>
2025-09-11 12:15:14 +0530

    add

---
+++ Dot net Core/Chapter 2/assess/hanneth/EmployeePayrollSystem/EmployeePayrollSystem/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollSystem
+{
+    public abstract class Employee
+    {
+       private string employeeid;
+       private string name;
+       private double basicSalary;
+           public string EmployeeId {  get { return employeeid; } set { employeeid = value; } }
+        public string Name { get { return name; } set { name = value; } }
+            public double BasicSalary { get; set; }
+        public Employee() { }
+        public Employee(string employeeid, string name, double basicSalary)
+        {
+            this.employeeid = employeeid;
+            Name = name;
+            BasicSalary = basicSalary;
+           
+        }
+       public void  GetEmployeeId()
+        {
+            Console.WriteLine("Enter employee id");
+            employeeid = Console.ReadLine();
+        }
+         public double GetBasicSalary()
+        {
+           Console.WriteLine("Enter basic salary");
+            basicSalary=Convert.ToInt64(Console.ReadLine());
+            return basicSalary;
+        }
+          public void GetName()
+        {
+            Console.WriteLine("Enter name");
+            name = Console.ReadLine();
+        }
+      public double SetBasicSalary(double Amount)
+        {
+            basicSalary=Amount;
+            return basicSalary;
+        }
+        public abstract void CalculateSalary();
+
+    }
+}
