297314aaadbfb1312f71147332dc608db51dc901
Neethu <neethukottoor@gmail.com>
2025-12-23 15:18:01 +0400

    added

---
+++ Dot net Core/Chapter 8/Students/Crud_MVC/Crud_MVC/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using Crud_MVC.Models;
+using Crud_MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentServicecs studentService;
+        public StudentController(IStudentServicecs studentServicecs)
+        {
+           studentService = studentServicecs;
+        }
+        public IActionResult Index()
+        {
+            var students=studentService.GetAllStudents();
+            return View(students);
+        }
+        public IActionResult Create() => View();
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            studentService.AddStudent(student);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var student=studentService.GetStudentById(id);
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            studentService.UpdateStudent(student);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var student=studentService.GetStudentById(id);
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            studentService.DeleteStudent(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Dot net Core/Chapter 8/Students/Crud_MVC/Crud_MVC/Repositories/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Crud_MVC.Models;
+
+namespace Crud_MVC.Repositories
+{
+    public interface IStudentRepository
+    {
+        public List<Student> GetAllStudents();
+        public void AddStudent(Student student);
+        public Student GetStudentById(int id);
+        public void UpdateStudent(Student student);
+        public void DeleteStudent(int id);
+    }
+}
